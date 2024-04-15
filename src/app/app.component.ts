8f46a761339c88961a1e237c8a3f03fe364970a1
Pratap Kotala <pk@yahoo.com>
2024-04-15 15:31:04 -0500

    Added @for block to display one course card for each course dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,8 @@ import { Course } from './model/course';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  csci160 = COURSES[0];
-  csci161 = COURSES[1];
-  csci213 = COURSES[2];
+  // csci160 = COURSES[0];
+  // csci161 = COURSES[1];
+  // csci213 = COURSES[2];
+  courses: Course[] = COURSES;
 }
